489334d2e026364d88b2ef10e756e0dcd1cf82b2
Muhammed Sazid Ul Bari <132262629+SazidWebDev@users.noreply.github.com>
2024-10-05 20:07:40 +0600

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,20 +4,20 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-      <meta name="description" content="The Hadith Stack" />
+      <meta name="description" content="TheHadithStack is a project aimed at providing a comprehensive collection of Hadiths (sayings and actions of Prophet Muhammad, peace be upon him). The project is implemented using an API to make access to Hadiths easy and organized." />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta name="robots" content="noindex, nofollow" />
       <meta name="googlebot" content="noindex, nofollow" />
       <meta property="og:image" content="https://i.ibb.co/gFC83dX/image.png" />
       <meta property="og:title" content="The Hadith Stack" />
-      <meta property="og:description" content="The Hadith Stack" />
+      <meta property="og:description" content="TheHadithStack is a project aimed at providing a comprehensive collection of Hadiths (sayings and actions of Prophet Muhammad, peace be upon him). The project is implemented using an API to make access to Hadiths easy and organized." />
       <meta property="og:url" content="https://thehadithstack.com" />
       <meta property="og:type" content="website" />
       <meta property="og:site_name" content="The Hadith Stack" />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:image" content="https://i.ibb.co/gFC83dX/image.png" />
       <meta name="twitter:title" content="The Hadith Stack" />
-      <meta name="twitter:description" content="The Hadith Stack" />
+      <meta name="twitter:description" content="TheHadithStack is a project aimed at providing a comprehensive collection of Hadiths (sayings and actions of Prophet Muhammad, peace be upon him). The project is implemented using an API to make access to Hadiths easy and organized." />
       <meta name="twitter:site" content="@thehadithstack" />
       <meta name="twitter:creator" content="@thehadithstack" />
       <meta name="twitter:domain" content="https://thehadithstack.com" />
